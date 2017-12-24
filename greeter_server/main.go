21accd495b2bb864d8b1880175bace0fdee28d6e
hanalaydrus <hanalaydrus@gmail.com>
2017-12-24 22:52:46 +0700

    delete server.go

--- greeter_server/main.go
+++ greeter_server/main.go
@@ -21,94 +21,52 @@
 package main
 
 import (
-	"flag"
 	"fmt"
-	"log"
 	"net/http"
-	// "net"
-	"os"
 
 	"google.golang.org/grpc"
-	// "google.golang.org/grpc/codes"
 	"google.golang.org/grpc/grpclog"
 	"google.golang.org/grpc/metadata"
-	// pb "google.golang.org/grpc/examples/helloworld/helloworld"
-	// "google.golang.org/grpc/reflection"
 
-	// "strings"
-
-	library "../helloworld"
+	pb "../helloworld"
 	"github.com/improbable-eng/grpc-web/go/grpcweb"
-	"golang.org/x/net/context"
+	"google.golang.org/grpc/reflection"
 )
-
-var (
-	enableTls       = flag.Bool("enable_tls", false, "Use TLS - required for HTTP2.")
-	tlsCertFilePath = flag.String("tls_cert_file", "../misc/localhost.crt", "Path to the CRT/PEM file.")
-	tlsKeyFilePath  = flag.String("tls_key_file", "../misc/localhost.key", "Path to the private key file.")
-)
-
-// const (
-// 	port = ":50051"
-// )
 
 // server is used to implement helloworld.GreeterServer.
 type server struct{}
 
 // SayHello implements helloworld.GreeterServer
-func (s *server) SayHello(ctx context.Context, in *library.HelloRequest) (*library.HelloReply, error) {
-	grpc.SendHeader(ctx, metadata.Pairs("Access-Control-Allow-Origin", "*"))
-	grpc.SetTrailer(ctx, metadata.Pairs("Post-Response-Metadata", "Is-sent-as-trailers-unary"))
-
-	return &library.HelloReply{Message: "Hello " + in.Name}, nil
+func (s *server) SayHello(in *pb.HelloRequest, stream pb.Greeter_SayHelloServer) error {
+	stream.SendHeader(metadata.Pairs("Pre-Response-Metadata", "Is-sent-as-headers-stream"))
+	helloReply := &pb.HelloReply{Message: "Hello " + in.Name}
+	for i := 0; i < 10; i++ {
+		if err := stream.Send(helloReply); err != nil {
+			return err
+		}
+	}
+	stream.SetTrailer(metadata.Pairs("Post-Response-Metadata", "Is-sent-as-trailers-stream"))
+	return nil
 }
 
-// func main() {
-// 	lis, err := net.Listen("tcp", port)
-// 	if err != nil {
-// 		log.Fatalf("failed to listen: %v", err)
-// 	}
-// 	s := grpc.NewServer()
-// 	pb.RegisterGreeterServer(s, &server{})
-// 	// Register reflection service on gRPC server.
-// 	reflection.Register(s)
-// 	if err := s.Serve(lis); err != nil {
-// 		log.Fatalf("failed to serve: %v", err)
-// 	}
-// }
-
 func main() {
-	flag.Parse()
+	port := 8080
+	s := grpc.NewServer()
+	pb.RegisterGreeterServer(s, &server{})
+	// Register reflection service on gRPC server.
+	reflection.Register(s)
 
-	port := 9090
-	if *enableTls {
-		port = 9091
-	}
-
-	grpcServer := grpc.NewServer()
-	library.RegisterGreeterServer(grpcServer, &server{})
-	grpclog.SetLogger(log.New(os.Stdout, "exampleserver: ", log.LstdFlags))
-	grpcweb.WithCorsForRegisteredEndpointsOnly(false)
-	// grpcweb.WithAllowedRequestHeaders("Access-Control-Allow-Headers")
-	wrappedServer := grpcweb.WrapServer(grpcServer)
+	wrappedServer := grpcweb.WrapServer(s, grpcweb.WithCorsForRegisteredEndpointsOnly(false))
 	handler := func(resp http.ResponseWriter, req *http.Request) {
 		wrappedServer.ServeHTTP(resp, req)
 	}
-
 	httpServer := http.Server{
 		Addr:    fmt.Sprintf(":%d", port),
 		Handler: http.HandlerFunc(handler),
 	}
+	grpclog.Printf("Starting server. http port: %d", port)
 
-	grpclog.Printf("Starting server. http port: %d, with TLS: %v", port, *enableTls)
-
-	if *enableTls {
-		if err := httpServer.ListenAndServeTLS(*tlsCertFilePath, *tlsKeyFilePath); err != nil {
-			grpclog.Fatalf("failed starting http2 server: %v", err)
-		}
-	} else {
-		if err := httpServer.ListenAndServe(); err != nil {
-			grpclog.Fatalf("failed starting http server: %v", err)
-		}
+	if err := httpServer.ListenAndServe(); err != nil {
+		grpclog.Fatalf("failed starting http server: %v", err)
 	}
 }
