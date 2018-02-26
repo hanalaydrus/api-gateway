ada253889f0ff0c5b67a8db8341fef738f71417f
hanalaydrus <hanalaydrus@gmail.com>
2018-02-26 23:05:38 +0700

    handle client disconnect, change protobuf contracts

--- Makefile
+++ Makefile
@@ -1,6 +1,8 @@
 build:
 	protoc -I semanticContract/ semanticContract/semanticContract.proto --go_out=plugins=grpc:semanticContract
 	protoc -I volumeContract/ volumeContract/volumeContract.proto --go_out=plugins=grpc:volumeContract
+	protoc -I densityContract/ densityContract/densityContract.proto --go_out=plugins=grpc:densityContract
+	protoc -I gatewayContract/ gatewayContract/gatewayContract.proto --go_out=plugins=grpc:gatewayContract
 
 run:
 	go run ./client_server.go
--- client_server.go
+++ client_server.go
@@ -19,21 +19,25 @@
 package main
 
 import (
-	// "fmt"
+	"fmt"
 	"time"
-	// "net/http"
+	"net/http"
 	"log"
 	"io"
+	"strings"
+	"strconv"
 
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
-	// "google.golang.org/grpc/grpclog"
-	// "google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/grpclog"
+	"google.golang.org/grpc/metadata"
 
 	pbs "./semanticContract"
-	// pbv "./volumeContract"
-	// "github.com/improbable-eng/grpc-web/go/grpcweb"
-	// "google.golang.org/grpc/reflection"
+	pbv "./volumeContract"
+	pbd "./densityContract"
+	pbg "./gatewayContract"
+	"github.com/improbable-eng/grpc-web/go/grpcweb"
+	"google.golang.org/grpc/reflection"
 )
 
 const (
@@ -42,154 +46,178 @@ const (
 	addressSemantic = "localhost:50049"
 )
 
-var count int32 = 0
-var prev_count = -1
-
 var timestamp = ""
 
 var density_state = "Lancar"
-var prev_density_state = "null"
 
 var semantic_state = ""
-var prev_semantic_state = "null"
 
 // server is used to implement helloworld.GreeterServer.
-// type server struct{}
+type server struct{}
 
-// SayHello implements helloworld.GreeterServer
-// func (s *server) SayHello(in *pb2.HelloRequest, stream pb2.Greeter_SayHelloServer) error {
-// 	stream.SendHeader(metadata.Pairs("Pre-Response-Metadata", "Is-sent-as-headers-stream"))
-// 	for {
-// 		if ((prev_count != count) || (prev_density_state != density_state)) {
-// 			helloReply := &pb2.HelloReply{Volume: count, Density: density_state}
-// 			if err := stream.Send(helloReply); err != nil {
-// 				return err
-// 			}
-// 			log.Println("Sending")
-// 			prev_count = count
-// 			prev_density_state = density_state
-// 		}
-// 	}
-// 	stream.SetTrailer(metadata.Pairs("Post-Response-Metadata", "Is-sent-as-trailers-stream"))
-// 	return nil
-// }
+func asClientDensity(id int32, resp chan string, ctx context.Context) {
+	var withBlock = grpc.WithBlock()
+	conn, erro := grpc.Dial(addressDensity, grpc.WithInsecure(), withBlock)
+	if erro != nil {
+		log.Printf("did not connect: %v", erro)
+	}
+	defer conn.Close()
+	c := pbd.NewGreeterClient(conn)
 
-// func asClientDensity() {
-// 	var withBlock = grpc.WithBlock()
-// 	conn, erro := grpc.Dial(addressDensity, grpc.WithInsecure(), withBlock)
-// 	if erro != nil {
-// 		log.Fatalf("did not connect: %v", erro)
-// 	}
-// 	defer conn.Close()
-// 	c := pb.NewGreeterClient(conn)
+	// Contact the server and print out its response.
 
-// 	// Contact the server and print out its response.
+	stream, erro := c.SayHello(context.Background(), &pbd.HelloRequest{Id: id})
+	if erro != nil {
+		log.Printf("could not greet: %v", erro)
+	}
+	for {
+		select{
+			case <- ctx.Done():
+				fmt.Println("close client")
+				return
+			default:
+				helloReply, erro := stream.Recv()
+				if erro == io.EOF {
+					return
+				}
+				if erro != nil {
+					log.Printf("%v.ListFeatures(_) = _, %v", c, erro)
+					return
+				}
+				resp <- helloReply.Response
+				// log.Println("Density: ", helloReply.Response)
+				time.Sleep(10 * time.Millisecond)
+		}
+	}
+}
 
-// 	stream, erro := c.SayHello(context.Background(), &pb.HelloRequest{Id: 1})
-// 	if erro != nil {
-// 		log.Fatalf("could not greet: %v", erro)
-// 	}
-// 	for {
-// 		helloReply, erro := stream.Recv()
-// 		if erro == io.EOF {
-// 			break
-// 		}
-// 		if erro != nil {
-// 			log.Fatalf("%v.ListFeatures(_) = _, %v", c, erro)
-// 		}
-// 		timestamp = helloReply.Timestamp
-// 		density_state = helloReply.Response
-// 		log.Println("Timestamp: ", timestamp)
-// 		log.Println("Density: ", density_state)
-// 		time.Sleep(10 * time.Millisecond)
-// 	}
-// }
+func asClientVolume(id int32, resp chan string, ctx context.Context) {
+	////////// Client ////////////
+	var withBlock = grpc.WithBlock()
+	conn, erro := grpc.Dial(addressVolume, grpc.WithInsecure(), withBlock)
+	if erro != nil {
+		log.Printf("did not connect: %v", erro)
+	}
+	defer conn.Close()
+	c := pbv.NewGreeterClient(conn)
 
-// func asClientVolume() {
-// 	////////// Client ////////////
-// 	var withBlock = grpc.WithBlock()
-// 	conn, erro := grpc.Dial(addressVolume, grpc.WithInsecure(), withBlock)
-// 	if erro != nil {
-// 		log.Fatalf("did not connect: %v", erro)
-// 	}
-// 	defer conn.Close()
-// 	c := pbv.NewGreeterClient(conn)
+	// Contact the server and print out its response.
+	stream, erro := c.SayHello(context.Background(), &pbv.HelloRequest{Id: id})
+	if erro != nil {
+		log.Printf("could not greet: %v", erro)
+	}
 
-// 	// Contact the server and print out its response.
-// 	stream, erro := c.SayHello(context.Background(), &pbv.HelloRequest{Id: 1})
-// 	if erro != nil {
-// 		log.Fatalf("could not greet: %v", erro)
-// 	}
-// 	for {
-// 		helloReply, erro := stream.Recv()
-// 		if erro == io.EOF {
-// 			break
-// 		}
-// 		if erro != nil {
-// 			log.Fatalf("%v.ListFeatures(_) = _, %v", c, erro)
-// 		}
-// 		count = helloReply.Volume
-// 		log.Println("Car Count: ", count)
-// 		time.Sleep(10 * time.Millisecond)
-// 	}
-// }
+	for {
+		select{
+			case <- ctx.Done():
+				fmt.Println("close client")
+				return
+			default:
+				helloReply, erro := stream.Recv()
+				if erro == io.EOF {
+					return
+				}
+				if erro != nil {
+					log.Printf("%v.ListFeatures(_) = _, %v", c, erro)
+					return
+				}
+				resp <- strconv.FormatInt(int64(helloReply.Volume),10)
+				// log.Println("Car Count: ", strconv.FormatInt(int64(helloReply.Volume),10))
+				
+				time.Sleep(10 * time.Millisecond)
+		}
+	}
+}
 
-func asClientSemantic() {
-	log.Println("tes")
+func asClientSemantic(id int32, resp chan string, ctx context.Context) {
 	var withBlock = grpc.WithBlock()
 	conn, erro := grpc.Dial(addressSemantic, grpc.WithInsecure(), withBlock)
 	if erro != nil {
-		log.Fatalf("did not connect: %v", erro)
+		log.Printf("did not connect: %v", erro)
 	}
 	defer conn.Close()
 	c := pbs.NewGreeterClient(conn)
 
 	// Contact the server and print out its response.
 
-	stream, erro := c.SayHello(context.Background(), &pbs.HelloRequest{Id: 1})
+	stream, erro := c.SayHello(context.Background(), &pbs.HelloRequest{Id: id})
 	if erro != nil {
-		log.Fatalf("could not greet: %v", erro)
+		log.Printf("could not greet: %v", erro)
 	}
 	for {
-		helloReply, erro := stream.Recv()
-		if erro == io.EOF {
-			break
+		select{
+			case <- ctx.Done():
+				fmt.Println("close client")
+				return
+			default:
+				helloReply, erro := stream.Recv()
+				if erro == io.EOF {
+					return
+				}
+				if erro != nil {
+					log.Printf("%v.ListFeatures(_) = _, %v", c, erro)
+					return
+				}
+				resp <- helloReply.Response
+				// log.Println("Semantic: ", helloReply.Response)
+				time.Sleep(10 * time.Millisecond)
 		}
-		if erro != nil {
-			log.Fatalf("%v.ListFeatures(_) = _, %v", c, erro)
-		}
-		semantic_state = helloReply.Response
-		log.Println("Semantic: ", semantic_state)
-		time.Sleep(10 * time.Millisecond)
 	}
 }
 
-// func asServer() {
-// 	//////////// Server //////////////
-// 	port := 8080
-// 	s := grpc.NewServer()
-// 	pb2.RegisterGreeterServer(s, &server{})
-// 	// Register reflection service on gRPC server.
-// 	reflection.Register(s)
+// SayHello implements helloworld.GreeterServer
+func (s *server) SayHello(in *pbg.HelloRequest, stream pbg.Greeter_SayHelloServer) error {
+	stream.SendHeader(metadata.Pairs("Pre-Response-Metadata", "Is-sent-as-headers-stream"))
+	ctx := stream.Context()
+	response := make(chan string)
+	if strings.ToLower(in.Type) == "volume" {
+		go asClientVolume(in.Id, response, ctx)
+	} else if strings.ToLower(in.Type) == "density" {
+		go asClientDensity(in.Id, response, ctx)
+	} else if strings.ToLower(in.Type) == "semantic" {
+		go asClientSemantic(in.Id, response, ctx)
+	}
+	for {
+		select{
+			case <- ctx.Done():
+				fmt.Println("close context")
+				return nil
+			default:
+				resp := <- response
+				helloReply := &pbg.HelloReply{Response: resp}
+				if err := stream.Send(helloReply); err != nil {
+					return err
+				}
+				fmt.Println("response : ",resp)
+		}
+	}
+	stream.SetTrailer(metadata.Pairs("Post-Response-Metadata", "Is-sent-as-trailers-stream"))
+	return nil
+}
 
-// 	wrappedServer := grpcweb.WrapServer(s, grpcweb.WithCorsForRegisteredEndpointsOnly(false))
-// 	handler := func(resp http.ResponseWriter, req *http.Request) {
-// 		wrappedServer.ServeHTTP(resp, req)
-// 	}
-// 	httpServer := http.Server{
-// 		Addr:    fmt.Sprintf(":%d", port),
-// 		Handler: http.HandlerFunc(handler),
-// 	}
-// 	grpclog.Printf("Starting server. http port: %d", port)
+func asServer() {
+	//////////// Server //////////////
+	port := 8080
+	s := grpc.NewServer()
+	pbg.RegisterGreeterServer(s, &server{})
+	// Register reflection service on gRPC server.
+	reflection.Register(s)
+
+	wrappedServer := grpcweb.WrapServer(s, grpcweb.WithCorsForRegisteredEndpointsOnly(false))
+	handler := func(resp http.ResponseWriter, req *http.Request) {
+		wrappedServer.ServeHTTP(resp, req)
+	}
+	httpServer := http.Server{
+		Addr:    fmt.Sprintf(":%d", port),
+		Handler: http.HandlerFunc(handler),
+	}
+	grpclog.Printf("Starting server. http port: %d", port)
 	
-// 	if err := httpServer.ListenAndServe(); err != nil {
-// 		grpclog.Fatalf("failed starting http server: %v", err)
-// 	}
-// }
+	if err := httpServer.ListenAndServe(); err != nil {
+		grpclog.Printf("failed starting http server: %v", err)
+	}
+}
 
 func main() {
-	// go asClientVolume()
-	// go asClientDensity()
-	asClientSemantic()
-	// asServer()
+	asServer()
 }
