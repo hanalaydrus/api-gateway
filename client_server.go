ce5b261a5c6bbf5f79e2cc2f200e1cab7348475b
hanalaydrus <hanalaydrus@gmail.com>
2018-09-25 07:38:31 +0700

    fix error

--- client_server.go
+++ client_server.go
@@ -23,22 +23,23 @@ import (
 	"time"
 	"net/http"
 	"log"
-	"io"
+	// "io"
 	"strings"
 	"strconv"
 	"encoding/json"
+	"sync"
 
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/grpclog"
 	"google.golang.org/grpc/metadata"
+	"github.com/improbable-eng/grpc-web/go/grpcweb"
+	// "google.golang.org/grpc/reflection"
 
 	pbs "api.gateway/semanticContract"
 	pbv "api.gateway/volumeContract"
 	pbd "api.gateway/densityContract"
 	pbg "api.gateway/gatewayContract"
-	"github.com/improbable-eng/grpc-web/go/grpcweb"
-	"google.golang.org/grpc/reflection"
 )
 
 const (
@@ -47,16 +48,12 @@ const (
 	addressSemantic = "semantic-service:50049"
 )
 
-var timestamp = ""
-
-var density_state = "Lancar"
-
-var semantic_state = ""
+var connection_count = 0
 
 // server is used to implement helloworld.GreeterServer.
 type server struct{}
 
-func asClientDensity(id int32, resp chan string, ctx context.Context) {
+func asClientDensity(id int32, resp chan string, ctx context.Context, wg sync.WaitGroup) {
 	var withBlock = grpc.WithBlock()
 	conn, erro := grpc.Dial(addressDensity, grpc.WithInsecure(), withBlock)
 	if erro != nil {
@@ -74,15 +71,20 @@ func asClientDensity(id int32, resp chan string, ctx context.Context) {
 	for {
 		select{
 			case <- ctx.Done():
-				fmt.Println("close client")
+				fmt.Println("close density client")
+				stream.CloseSend()
+				conn.Close()
+				close(resp)
+				wg.Done()
 				return
 			default:
 				helloReply, erro := stream.Recv()
-				if erro == io.EOF {
-					return
-				}
 				if erro != nil {
 					log.Printf("%v.ListFeatures(_) = _, %v", c, erro)
+					stream.CloseSend()
+					conn.Close()
+					close(resp)
+					wg.Done()
 					return
 				}
 				resp <- helloReply.Response
@@ -92,7 +94,7 @@ func asClientDensity(id int32, resp chan string, ctx context.Context) {
 	}
 }
 
-func asClientVolume(id int32, resp chan string, ctx context.Context) {
+func asClientVolume(id int32, resp chan string, ctx context.Context, wg sync.WaitGroup) {
 	////////// Client ////////////
 	var withBlock = grpc.WithBlock()
 	conn, erro := grpc.Dial(addressVolume, grpc.WithInsecure(), withBlock)
@@ -111,15 +113,20 @@ func asClientVolume(id int32, resp chan string, ctx context.Context) {
 	for {
 		select{
 			case <- ctx.Done():
-				fmt.Println("close client")
+				fmt.Println("close volume client")
+				stream.CloseSend()
+				conn.Close()
+				close(resp)
+				wg.Done()
 				return
 			default:
 				helloReply, erro := stream.Recv()
-				if erro == io.EOF {
-					return
-				}
 				if erro != nil {
-					log.Printf("%v.ListFeatures(_) = _, %v", c, erro)
+					fmt.Println("%v.ListFeatures(_) = _, %v", c, erro)
+					stream.CloseSend()
+					conn.Close()
+					close(resp)
+					wg.Done()
 					return
 				}
 				resp <- strconv.FormatInt(int64(helloReply.Volume),10)
@@ -130,7 +137,7 @@ func asClientVolume(id int32, resp chan string, ctx context.Context) {
 	}
 }
 
-func asClientSemantic(id int32, resp chan string, ctx context.Context) {
+func asClientSemantic(id int32, resp chan string, ctx context.Context, wg sync.WaitGroup) {
 	var withBlock = grpc.WithBlock()
 	conn, erro := grpc.Dial(addressSemantic, grpc.WithInsecure(), withBlock)
 	if erro != nil {
@@ -148,15 +155,20 @@ func asClientSemantic(id int32, resp chan string, ctx context.Context) {
 	for {
 		select{
 			case <- ctx.Done():
-				fmt.Println("close client")
+				fmt.Println("close semantic client")
+				stream.CloseSend()
+				conn.Close()
+				close(resp)
+				wg.Done()
 				return
 			default:
 				helloReply, erro := stream.Recv()
-				if erro == io.EOF {
-					return
-				}
 				if erro != nil {
-					log.Printf("%v.ListFeatures(_) = _, %v", c, erro)
+					fmt.Println("%v.ListFeatures(_) = _, %v", c, erro)
+					stream.CloseSend()
+					conn.Close()
+					close(resp)
+					wg.Done()
 					return
 				}
 				resp <- helloReply.Response
@@ -166,63 +178,102 @@ func asClientSemantic(id int32, resp chan string, ctx context.Context) {
 	}
 }
 
-// SayHello implements helloworld.GreeterServer
-func (s *server) SayHello(in *pbg.HelloRequest, stream pbg.Greeter_SayHelloServer) error {
-	stream.SendHeader(metadata.Pairs("Pre-Response-Metadata", "Is-sent-as-headers-stream"))
-	ctx := stream.Context()
-	response := make(chan string)
-	responseVolume := make(chan string)
-	responseDensity := make(chan string)
-	responseSemantic := make(chan string)
-
-	if strings.ToLower(in.Type) == "volume" {
-		go asClientVolume(in.Id, response, ctx)
-	} else if strings.ToLower(in.Type) == "density" {
-		go asClientDensity(in.Id, response, ctx)
-	} else if strings.ToLower(in.Type) == "semantic" {
-		go asClientSemantic(in.Id, response, ctx)
-	} else if strings.ToLower(in.Type) == "all" {
-		go asClientVolume(in.Id, responseVolume, ctx)
-		go asClientDensity(in.Id, responseDensity, ctx)
-		go asClientSemantic(in.Id, responseSemantic, ctx)
-	}
+func serveRequest (stream pbg.Greeter_SayHelloServer, response chan string, ctx context.Context, wg sync.WaitGroup) {
 	for {
-		select{
+		select {
 			case <- ctx.Done():
 				fmt.Println("close context")
-				return nil
+				wg.Done()
+				return
 			default:
-				if strings.ToLower(in.Type) == "all" {
-					type Message struct {
-						Volume string `json:"volume"`
-						Density string `json:"density"`
-						Semantic string `json:"semantic"`
-					}
-					respVolume := <- responseVolume
-					respDensity := <- responseDensity
-					respSemantic := <- responseSemantic
-
-					m := Message{Volume: respVolume, Density: respDensity, Semantic: respSemantic}
-					b, err := json.Marshal(m)
-					if err != nil {
-				        fmt.Printf("Error: %s", err)
-				        return err
-				    }
-
-					helloReply := &pbg.HelloReply{Response: string(b)}
-					if err := stream.Send(helloReply); err != nil {
-						return err
-					}
-				} else {
-					resp := <- response
-					helloReply := &pbg.HelloReply{Response: resp}
-					if err := stream.Send(helloReply); err != nil {
-						return err
-					}
+				resp := <- response
+				helloReply := &pbg.HelloReply{Response: resp}
+				if err := stream.Send(helloReply); err != nil {
+					// fmt.Printf("Error: %s", err)
+					continue
 				}
 		}
 	}
+}
+
+func serveRequestAll (stream pbg.Greeter_SayHelloServer, responseVolume chan string, responseDensity chan string, responseSemantic chan string, ctx context.Context, wg sync.WaitGroup) {
+	for {
+		select {
+			case <- ctx.Done():
+				fmt.Println("close context")
+				wg.Done()
+				return
+			default:
+				type Message struct {
+					Volume string `json:"volume"`
+					Density string `json:"density"`
+					Semantic string `json:"semantic"`
+				}
+				respVolume := <- responseVolume
+				respDensity := <- responseDensity
+				respSemantic := <- responseSemantic
+
+				m := Message{Volume: respVolume, Density: respDensity, Semantic: respSemantic}
+				b, err := json.Marshal(m)
+				if err != nil {
+			        fmt.Println("Error: %s", err)
+			        continue
+			    }
+
+				helloReply := &pbg.HelloReply{Response: string(b)}
+				if err := stream.Send(helloReply); err != nil {
+					fmt.Println("Error: %s", err)
+					continue
+				}
+		}
+	}
+}
+
+// SayHello implements helloworld.GreeterServer
+func (s *server) SayHello(in *pbg.HelloRequest, stream pbg.Greeter_SayHelloServer) error {
+	var wg sync.WaitGroup
+	stream.SendHeader(metadata.Pairs("Pre-Response-Metadata", "Is-sent-as-headers-stream"))
 	stream.SetTrailer(metadata.Pairs("Post-Response-Metadata", "Is-sent-as-trailers-stream"))
+	ctx := stream.Context()
+
+	if strings.ToLower(in.Type) == "volume" {
+
+		response := make(chan string)
+		wg.Add(2)
+		go asClientVolume(in.Id, response, ctx, wg)
+		go serveRequest(stream, response, ctx, wg)
+		wg.Wait()
+
+	} else if strings.ToLower(in.Type) == "density" {
+
+		response := make(chan string)
+		wg.Add(2)
+		go asClientDensity(in.Id, response, ctx, wg)
+		go serveRequest(stream, response, ctx, wg)
+		wg.Wait()
+
+	} else if strings.ToLower(in.Type) == "semantic" {
+
+		response := make(chan string)
+		wg.Add(2)
+		go asClientSemantic(in.Id, response, ctx, wg)
+		go serveRequest(stream, response, ctx, wg)
+		wg.Wait()
+
+	} else if strings.ToLower(in.Type) == "all" {
+
+		responseVolume := make(chan string)
+		responseDensity := make(chan string)
+		responseSemantic := make(chan string)
+
+		wg.Add(4)
+		go asClientVolume(in.Id, responseVolume, ctx, wg)
+		go asClientDensity(in.Id, responseDensity, ctx, wg)
+		go asClientSemantic(in.Id, responseSemantic, ctx, wg)
+		go serveRequestAll(stream, responseVolume, responseDensity, responseSemantic, ctx, wg)
+		wg.Wait()
+	}
+
 	return nil
 }
 
@@ -232,7 +283,7 @@ func asServer() {
 	s := grpc.NewServer()
 	pbg.RegisterGreeterServer(s, &server{})
 	// Register reflection service on gRPC server.
-	reflection.Register(s)
+	// reflection.Register(s)
 
 	wrappedServer := grpcweb.WrapServer(s, grpcweb.WithCorsForRegisteredEndpointsOnly(false))
 	handler := func(resp http.ResponseWriter, req *http.Request) {
